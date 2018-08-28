740d79fbe385b4678d557abddeea28b5b52a933a
Alessio Caligiuri <alessio.caligiuri@gmail.com>
2018-08-28 23:25:13 +0200

    Added project with FreeRTOS.

--- STM32_CubeMx/STM32_AutoImporter.bat
+++ STM32_CubeMx/STM32_AutoImporter.bat
@@ -4,5 +4,5 @@ C:
 
 cd C:\STM32Toolchain\CubeMXImporter
 
-cubemximporter.py D:\INGEGNERIA\RF\Progetto\STM32_Workspace\STM32F103_Blink D:\INGEGNERIA\RF\Progetto\STM32_CubeMx\STM32F103_Blink
+cubemximporter.py D:\INGEGNERIA\RF\Progetto\STM32_Workspace\STM32F103_RTOS_Test D:\INGEGNERIA\RF\Progetto\STM32_CubeMx\STM32F103_RTOS_Test 
 PAUSE
