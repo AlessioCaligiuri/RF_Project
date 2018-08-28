d045ed1c19b226aa9518adbfdcee6e5a68518f97
Alessio Caligiuri <alessio.caligiuri@gmail.com>
2018-08-28 21:07:08 +0200

    Added STM32 project for Eclipse and for CubeMX

---
+++ STM32_CubeMx/STM32_AutoImporter.bat
@@ -0,0 +1,8 @@
+ECHO Script per copiare automaticamente un progetto CubeMx nel progetto Eclipse.
+
+C:
+
+cd C:\STM32Toolchain\CubeMXImporter
+
+cubemximporter.py D:\INGEGNERIA\RF\Progetto\STM32_Workspace\STM32F103_Blink D:\INGEGNERIA\RF\Progetto\STM32_CubeMx\STM32F103_Blink
+PAUSE
